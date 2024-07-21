eb804ce127b4d1b7361b0787a6648d33436a357d
Rajesh <itsgamechanger10@gmail.com>
2024-07-21 22:35:33 +0530

    datatypes

--- js_basics/test2.js
+++ js_basics/test2.js
@@ -1,2 +1,29 @@
-var name="rajesh pachauri"
-console.log(name);
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser as it is a hole document
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("raj")
+
+
+let name = "rajesh"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqueness
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
